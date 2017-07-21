22bc09b33e14df0372e26693365032c1565e810b
frebern <frebern@naver.com>
2017-07-21 18:33:37 +0900

    [07/21] PDPServer Refactoring

--- src/main/java/httpServer/PDPServer.java
+++ src/main/java/httpServer/PDPServer.java
@@ -47,18 +47,49 @@ public class PDPServer {
                 JsonObject inputJson = gson.fromJson(inputString, JsonObject.class);
                 String requestBody = inputJson.get("body").getAsString();
                 //TODO: policiesId 를 이용해 policies 찾는 과정을 고민해야함
+                // 1. json에서 policy id 가져와서(여기 policy id였나 PEP id였나..?)
                 String policiesId = null;
                 if (inputJson.get("policyId") != null)
                     policiesId = inputJson.get("policyId").getAsString();
-                String sep = File.separator;
-                String policies = (new File(".")).getCanonicalPath() + sep + "resources" + sep + "IntentConflictExamplePolicies";
-                //TODO: attribute set 을 어떻게 가져올 건지에 대한 고민 필요
-                String response = evaluateRequest(requestBody, policies);
+                // 2. 해당 경로 DB로 부터 검색해서 가져오고 PDP 호출
+                String response = evaluateRequest(requestBody, getPolicyPathFromId("IntentConflictExamplePolicies"));
+                // 3. 결과 리턴.
                 httpResponse(httpExchange, response);
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        // DB에서 Search 여기 현재 미구현상태.
+        private String getPolicyPathFromId(String policiesId) throws IOException{
+            return getPolicyPathFromName("IntentConflictExapmle");
+        }
+
+        // 우선 ./resources/를 Base 디렉토리로 설정.
+        private String getPolicyPathFromName(String dirName) throws IOException{
+            final String BASE = "resources";
+            final String SEP = File.separator;
+            // ./resources/{dirName}
+            String path = (new File(".")).getCanonicalPath() + SEP + BASE + SEP + dirName;
+            return new File(path).exists() ? path : null;
+        }
+
+        //
+        private String evaluateRequest(String request, String ...policiesLoc) {
+            if (request.equals("")) return null;
+            String response = pdpInterface.evaluate(request, policiesLoc);
+            return response;
+        }
+
+        private String read(InputStream inputStream) throws IOException {
+            StringBuffer sb = new StringBuffer();
+            byte[] b = new byte[4096];
+            while (inputStream.available() != 0) {
+                int n = inputStream.read(b);
+                sb.append(new String(b, 0, n));
+            }
+            return sb.toString();
         }
 
         private void httpResponse(HttpExchange httpExchange, String response){
@@ -74,22 +105,6 @@ public class PDPServer {
             } catch(IOException e) {
                 e.printStackTrace();
             }
-        }
-
-        private String read(InputStream inputStream) throws IOException {
-            StringBuffer sb = new StringBuffer();
-            byte[] b = new byte[4096];
-            while (inputStream.available() != 0) {
-                int n = inputStream.read(b);
-                sb.append(new String(b, 0, n));
-            }
-            return sb.toString();
-        }
-
-        private String evaluateRequest(String request, String ...policiesLoc) {
-            if (request.equals("")) return null;
-            String response = pdpInterface.evaluate(request, policiesLoc);
-            return response;
         }
 
         public void handleError(HttpExchange httpExchange, String errMsg) throws IOException {
